efe92a0ed3dcf9f60866b26123ea9bc55c5f3e84
Devin Smith <smithdevin6@gmail.com>
2022-10-28 14:15:59 -0600

    changed to windows only app

--- GbaWindow.py
+++ GbaWindow.py
@@ -1,14 +1,36 @@
 import time
 import subprocess
+import win32gui
+import win32process
 import pyautogui
 import pydirectinput
 
 class GbaWindow:
     def __init__(self):
-        self.gba = subprocess.Popen("") 
+        self.gba = subprocess.Popen("C:\\Users\\moonc\\OneDrive\\Desktop\\Games\\VisualBoyAdvance.exe") 
         self.hwnd = None
 
         time.sleep(.5)
+        self.findGbaWindow()
+        self.resizeWindow()
+
+    def findGbaWindow(self):
+        def callback (hwnd, hwnds):
+            if win32gui.IsWindowVisible (hwnd) and win32gui.IsWindowEnabled (hwnd):
+                _, found_pid = win32process.GetWindowThreadProcessId (hwnd)
+
+                if found_pid == self.gba.pid:
+                    hwnds.append (hwnd)
+
+            return True
+        
+        # Make a list of py handle windows and get the one that matches our pid
+        hwnds = []
+        win32gui.EnumWindows(callback, hwnds)
+        self.hwnd = hwnds[0]
+
+    def resizeWindow(self):
+        win32gui.MoveWindow(self.hwnd, 1713, 0, 1735, 1440, True)
 
     def openGame(self):
         with pyautogui.hold('ctrl'):
